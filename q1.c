f8bcbc37df5bbe70a0f4ffcb293bd6b6c8dec383
sleepyfig <ameerakhatri@gmail.com>
2026-09-15 02:12:43 +0500

    second commit

--- q1.c
+++ q1.c
@@ -1,50 +1,59 @@
 #include <stdio.h>
-int main(){
-    int season, roomType, noOfNights;
-    float price, totalPrice;
-    totalPrice = 0;
-    price = 0;
-    printf("enter season(1 for peak and 2 for off-peak): ");
-    scanf("%d", &season);
-    printf("enter room type(1 for standard, 2 for deluxe, 3 for suite): ");
-    scanf("%d", &roomType);
-    printf("how many nights have u been in the hotel: ");
-    scanf("%d",&noOfNights);
-    if (season == 1){
-        switch(roomType){
-            case 1:
-                price = 5000;
-                break;
-            case 2:
-                price = 8000;
-                break;
-            case 3:
-                price = 12000;
-                break;
-            default:
-                printf("invalid input!\n");
-        };
-    } else if(season == 2) {
-        switch(roomType){
-            case 1:
-                price = 3000;
-                break;
-            case 2:
-                price = 5000;
-                break;
-            case 3:
-                price = 8000;
-                break;
-            default:
-                printf("invalid room type input!\n");
-        };
-    } else {
-        printf("invalid season input!\n");
-    };
-    totalPrice = price * noOfNights;
-    if (noOfNights > 7){
-        totalPrice = totalPrice * 0.85;
-    };
 
-    printf("total price: %.2f\n", totalPrice);
+int main() {
+    int season, roomType, nights;
+    float rate = 0, discount = 0, guestTotal, totalRevenue = 0;
+
+    printf("Seasons\n 1 = Peak\n 2 = Off-Peak\n Enter season (1 or 2): ");
+    scanf("%d", &season);
+    printf("Room Types\n 1 = Standard\n 2 = Deluxe\n 3 = Suite\n Enter room type (1, 2, or 3): ");
+    scanf("%d", &roomType);
+    printf("Enter number of nights: ");
+    scanf("%d", &nights);
+
+    if (nights <= 0) {
+        printf("Invalid number of nights!\n");
+        return 1;
+    }
+
+    if (season == 1) {
+        switch (roomType) {
+            case 1: rate = 5000;  break;
+            case 2: rate = 8000;  break;
+            case 3: rate = 12000; break;
+            default:
+                printf("Invalid room type!\n");
+                return 1;
+        }
+    } else if (season == 2) {
+        switch (roomType) {
+            case 1: rate = 3000; break;
+            case 2: rate = 5000; break;
+            case 3: rate = 8000; break;
+            default:
+                printf("Invalid room type!\n");
+                return 1;
+        }
+    } else {
+        printf("Invalid season!\n");
+        return 1;
+    }
+
+    guestTotal = rate * nights;
+
+    if (nights > 7) {
+        discount = guestTotal * 0.15;
+    } else {
+        discount = 0;
+    }
+
+    guestTotal = guestTotal - discount;
+    totalRevenue = totalRevenue + guestTotal;
+
+    printf("Rate per night : Rs. %.2f\n", rate);
+    printf("Discount       : Rs. %.2f\n", discount);
+    printf("Guest total    : Rs. %.2f\n", guestTotal);
+    printf("Hotel Total Revenue: Rs. %.2f\n", totalRevenue);
+
+    return 0;
 }
